e5bce28dd9dda64f4d5ff80252945fbe11092171
Dheeraj Sharma <at60sharwan@gmail.com>
2024-10-25 15:18:02 +0530

    Author card detail and images changed

--- src/components/shared/PodcastSection/PodcastCardExp.tsx
+++ src/components/shared/PodcastSection/PodcastCardExp.tsx
@@ -95,7 +95,7 @@ const PodcastCardExp = ({
     };
   }, [setCurrentlyPlaying]);
   return (
-    <div className='relative  bg-white w-full border border-zinc-100/20 rounded-3xl   p-4 transition-all text-black  hover:scale-105  duration-300'>
+    <div className="relative  bg-white w-full border border-zinc-100/20 rounded-3xl   p-4 transition-all text-black  hover:scale-105  duration-300">
       {/* <div className=' absolute h-full w-full top-0 left-0 bg-[#ffffff76] opacity-0  transition-all duration-300 hover:opacity-100 hover:backdrop-blur-sm '> */}
       {/* <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2   text-sm font-semibold text-center flex gap-4 items-center justify-center '>
           <button
@@ -128,72 +128,73 @@ const PodcastCardExp = ({
           )}
         </div> */}
       {/* </div> */}
-      <div className='text-2xl font-bold text-black mb-1 line-clamp-1 flex items-center justify-between '>
-        <span className='truncate pb-2 '>{podcast.topic}</span>
+      <div className="text-2xl font-bold text-black mb-1 line-clamp-1 flex items-center justify-between ">
+        <span className="truncate pb-2 ">{podcast.topic}</span>
         {currentlyPlaying === audioRef.current && isPlaying && (
           <span
-            className=' px-2 h-6 text-xs text-black bg-red-500 leading-tight font-medium flex gap-2 items-center justify-between
-            rounded-full whitespace-nowrap'
+            className=" px-2 h-6 text-xs text-black bg-red-500 leading-tight font-medium flex gap-2 items-center justify-between
+            rounded-full whitespace-nowrap"
           >
-            <div className='rotate-90'>
+            <div className="rotate-90">
               <FaAlignCenter />
             </div>
           </span>
         )}
       </div>
-      <div className='h-48 overflow-hidden rounded-[12px] mb-3 '>
+      <div className="h-48 overflow-hidden rounded-[12px] mb-3 ">
         <img
           src={podcast.thumbnail}
-          alt='Podcast thumbnail'
-          className='w-full object-cover'
+          alt="Podcast thumbnail"
+          className="w-full object-cover"
         />
       </div>
 
-      <p className='text-sm text-black line-clamp-3'>{podcast.description}</p>
-      <div className=' flex gap-3 items-center  justify-between text-xs my-2 mb-0'>
-        <div className='flex items-center gap-2 '>
-          <div className='w-6 rounded-full bg-red-600 border-2 border-primary '>
-            <img
-              src={
-                podcast.author === "Kisha Kothari"
-                  ? "/assets/images/kisha.jpg"
-                  : "/assets/images/harris illustration.jpg"
-              }
-              alt='Author image'
-              className=' rounded-full'
-            />
-          </div>
+      <p className="text-sm text-black line-clamp-3">{podcast.description}</p>
+      <div className=" flex gap-3 items-center  justify-between text-xs my-2 mb-0">
+        <div className="flex items-center gap-2 ">
           <TooltipProvider>
             <Tooltip>
               <TooltipTrigger asChild>
-                <span className='text-primary font-bold  text-base'>
-                  {podcast.author}
-                </span>
-              </TooltipTrigger>
-              <TooltipContent className='overflow-visible flex items-start gap-2 border border-black '>
-                <div className='w-12 rounded-full bg-red-600 border-2 border-primary '>
+                <div className="w-6 h-6 rounded-full bg-red-600 border-2 border-primary  flex gap-3 items-center ">
                   <img
-                    src='/assets/images/kisha.jpg'
-                    alt='Author image'
-                    className='w-full object-cover rounded-full'
+                    src={
+                      podcast.author === "Kisha Kothari"
+                        ? "/assets/images/kisha.jpg"
+                        : "/assets/images/harris-illustration.jpg"
+                    }
+                    alt="Author image"
+                    className=" rounded-full"
                   />
-                </div>
-                <div className='flex flex-col gap-0 '>
-                  <span className='text-primary text-base tracking-tighter leading-none'>
+                  <span className="text-primary font-bold  text-base whitespace-nowrap">
                     {podcast.author}
                   </span>
-                  <span className='text-primary text-sm '>
-                    (Psychology Intern)
+                </div>
+              </TooltipTrigger>
+              <TooltipContent className="overflow-visible flex items-start gap-2   ">
+                <div className="w-12 rounded-full bg-red-600 border-2 border-primary ">
+                  <img
+                    src={
+                      podcast.author === "Kisha Kothari"
+                        ? "/assets/images/kisha.jpg"
+                        : "/assets/images/harris-illustration.jpg"
+                    }
+                    alt="Author image"
+                    className=" rounded-full"
+                  />
+                </div>
+                <div className="flex flex-col gap-0  items-center">
+                  <span className="text-primary text-base tracking-tighter leading-none">
+                    {podcast.author}
                   </span>
                 </div>
               </TooltipContent>
             </Tooltip>
           </TooltipProvider>
         </div>
-        <div className='flex items-center justify-between '>
+        <div className="flex items-center justify-between ">
           <button
             onClick={handleSamplePlay}
-            className=' bg-rose-600  rounded-full px-4 py-2 text-white  flex gap-2 items-center justify-center whitespace-nowrap hover:bg-rose-700 transition-all duration-300'
+            className=" bg-rose-600  rounded-full px-4 py-2 text-white  flex gap-2 items-center justify-center whitespace-nowrap hover:bg-rose-700 transition-all duration-300"
           >
             {isPlaying ? "Pause" : "Listen Now"}
             {currentlyPlaying === audioRef.current && isPlaying ? (
